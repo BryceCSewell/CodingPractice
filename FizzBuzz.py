2996eea222a78f7e9b54966cd2900a1fc89fb39a
Bryce Sewell <104529181+BryceCSewell@users.noreply.github.com>
2023-10-13 12:10:25 -0500

    Create FizzBuzz.py
    Age old FizzBuzz challenge utilizing while loops and if statements

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+integers = input("Enter integers for x, y, and n: ").split()
+integers = [eval(i) for i in integers] #splits string inputs into integers
+
+x = integers[0]
+y = integers[1]
+n = integers[2]
+
+def main():
+    count = 1
+    while count <= n:
+        if (count % x == 0) and (count % y != 0):
+            print("Fizz")
+        elif (count % y == 0) and (count % x != 0):
+            print("Buzz")
+        elif (count % x == 0) and (count % y == 0):
+            print("FizzBuzz")
+        else:
+            print(count)
+        count += 1
+
+if __name__ == "__main__":
+    main()
