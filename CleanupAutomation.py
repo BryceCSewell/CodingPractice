373ed3e8faa9e71ade722e4526cc5b949fa3998a
Bryce Sewell <104529181+BryceCSewell@users.noreply.github.com>
2024-09-16 21:05:08 -0500

    Upload CleanupAutomation.py

---
+++ CleanupAutomation.py
@@ -0,0 +1,64 @@
+import os
+import time
+import logging
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+source_dir = "/Users/bryce/Downloads"
+dest_dir_sfx = "/Users/bryce/Downloads/Sound"
+dest_dir_music = "/Users/bryce/Downloads/Music"
+dest_dir_video = "/Users/bryce/Downloads/Video"
+dest_dir_image = "/Users/bryce/Downloads/Image"
+
+def makeUnique(path):
+    filename, extenstion = os.path.splitext(path)
+    counter = 1
+    while os.path.exists(path):
+        path = filename + " (" + str(counter) + ")" + extenstion
+        counter += 1
+
+    return path
+
+def move(dest, entry, name):
+    file_exists = os.path.exists(dest + "/" + name)
+    if file_exists:
+        unique_name = makeUnique(name)
+        os.rename(entry, unique_name)
+    shutil.move(entry, dest)
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with os.scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                dest = source_dir
+                if name.endswith('.wav') or name.endswith('.mp3'):
+                    if entry.stat().st_size < 25000000 or "SFX" in name: #differentiates between regular music and sound effects
+                        dest = dest_dir_sfx
+                    else:
+                        dest = dest_dir_music
+                    move(dest, entry, name)
+                elif name.endswith('.mov') or name.endswith('.mp4'):
+                    dest = dest_dir_video
+                    move(dest, entry, name)
+                elif name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.png'):
+                    dest = dest_dir_image
+                    move(dest, entry, name)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format = '%(asctime)s - %(message)s',
+                        datefmt = '%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
